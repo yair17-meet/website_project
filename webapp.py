267cacd614858227782002e53c9e5a3ac89426b0
eden18-meet <edenv200@gmail.com>
2017-02-10 19:42:27 +0200

    nothing

--- webapp.py
+++ webapp.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from model import *
-
+from sqlalchemy import Column,Integer,String, DateTime, ForeignKey, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine, func
+from passlib.apps import custom_app_context as pwd_context
 
 app = Flask(__name__)
 
 
-engine = create_angine('sqlite:///webdata.db')
+engine = create_engine('sqlite:///webdata.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine, autoflush=False)
 session = DBSession()
@@ -15,4 +19,8 @@ session = DBSession()
 
 @app.route('/')
 def hello_world():
-	return 
+	return 'main page route'
+
+
+if __name__=='__main__':
+	app.run()
